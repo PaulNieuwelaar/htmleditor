879d503b56c9096a078610be6dd590d76953c8d5
Paul Nieuwelaar <14048382+PaulNieuwelaar@users.noreply.github.com>
2018-01-11 10:54:54 +1300

    Updated CKEditor and JQuery to latest versions

--- HTMLEditor/HTMLEditor.Web/mag_/htmleditor/config.js
+++ HTMLEditor/HTMLEditor.Web/mag_/htmleditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -21,7 +21,8 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'styles' },
 		{ name: 'colors' },
 		{ name: 'tools' },
-		{ name: 'others' }
+		{ name: 'others' },
+		{ name: 'about' }
 	];
 
 	// The default plugins included in the basic setup define some buttons that
